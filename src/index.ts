1418da78836ba3f6ab745031d5827c93455f05ae
StepanAndrosov <stapan.news@yandex.ru>
2024-07-06 05:27:58 +0300

    init socket app

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const port = process.env.PORT || 5000;
+
+export const app = express();
+app.use(express.json());
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: 'http://localhost:5173' },
+});
+
+export async function startApp() {
+
+  app.get('/', (req, res) => {
+    res.send('Hello!');
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`${socket.id} user connected`);
+
+    socket.on('disconnect', () => {
+      console.log(`${socket.id}  disconnected`);
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log(msg)
+      io.emit('chat message', msg);
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
+}
+
+startApp();
